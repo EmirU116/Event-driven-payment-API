2fd472095e9d9b2a4c96c2d1083f66ccccf002cb
Emir Ulusoy <emir_ulusoy_49@hotmail.com>
2025-06-01 13:54:32 +0200

    wrote input data of the card that should be sent

---
+++ src/card-swipe/creditcard/CardData.cs
@@ -0,0 +1,10 @@
+
+public class CardData
+{
+    public string? userCardName { get; set; }
+    public int cardNumber { get; set; }
+    public int cardCVV { get; set; }
+    public string? cardBank { get; set; }
+    public int expireData { get; set; }
+}
+    

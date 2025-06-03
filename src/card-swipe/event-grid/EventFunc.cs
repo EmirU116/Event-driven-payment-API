c3714a1b779e1af4f04acd8f8ed2787a64c49c0b
Emir Ulusoy <emir_ulusoy_49@hotmail.com>
2025-06-03 11:45:28 +0200

    starting with event grid function and added some packages for it

---
+++ src/card-swipe/event-grid/EventFunc.cs
@@ -0,0 +1,9 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Extensions.Logging;
+using Azure.Messaging.EventGrid;
+
+public class EventFunc
+{
+    
+}
